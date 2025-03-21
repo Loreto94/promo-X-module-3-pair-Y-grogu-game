b5dd089822a37b604f1c95524096f627213ad338
Loreto <loreto0105@gmail.com>
2025-03-21 10:43:00 +0100

    Main structure Grogu

---
+++ src/components/App.jsx
@@ -0,0 +1,55 @@
+import "../scss/App.scss";
+
+
+function App() {
+    return (
+        <div>
+            <header>
+                <h1>¡Cuidado con Grogu!</h1>
+            </header>
+            <main class="page">
+                <section class="board">
+                    <div class="cell"><div class="grogu">👣</div></div>
+                    <div class="cell"></div>
+                    <div class="cell"></div>
+                    <div class="cell"></div>
+                    <div class="cell"></div>
+                    <div class="cell"></div>
+                    <div class="cell"></div>
+                </section>
+
+                <section>
+                    <button class="dice">Lanzar Dado</button>
+                    <div class="game-status">En curso</div>
+                </section>
+
+                <section class="goods-container">
+                    <div class="goods-item">🍪</div>
+                    <div class="goods-item">🍪</div>
+                    <div class="goods-item">🍪</div>
+                </section>
+                <section class="goods-container">
+                    <div class="goods-item">🥚</div>
+                    <div class="goods-item">🥚</div>
+                    <div class="goods-item">🥚</div>
+                </section>
+                <section class="goods-container">
+                    <div class="goods-item">🐸</div>
+                    <div class="goods-item">🐸</div>
+                    <div class="goods-item">🐸</div>
+                </section>
+                <section>
+                    <button class="restart-button">Reiniciar Juego</button>
+                </section>
+            </main>
+        </div>
+
+
+
+
+    );
+}
+
+export default App
+
+
