902e24ce713ff480ea1428ef43191b26e6f22f18
Loreto <loreto0105@gmail.com>
2025-03-27 11:13:56 +0100

    mapBoard

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,18 +1,31 @@
 import "../scss/App.scss";
 import Grogu from "./Grogu.jsx";
 import "../scss/components/Board.scss";
+import { useState } from "react";
 
-function Board (){
-    return(
-    <section className="board">
-        <div className="cell"><Grogu/></div>
-        <div className="cell"></div>
-        <div className="cell"></div>
-        <div className="cell"></div>
-        <div className="cell"></div>
-        <div className="cell"></div>
-        <div className="cell"></div>
-    </section>
-    )  
+function Board() {
+    return (
+        <section className="board">
+            {
+                Array(7).fill().map(() => {
+                    return (
+                        <div className="cell"></div>
+                    );
+                })
+            }
+
+        </section>
+    )
 }
-export default Board
+export default Board;
+
+
+{/*<section className="board">
+        <div className="cell"><Grogu/></div>
+       <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+    </section>*/}
